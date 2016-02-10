8652e89fc8706f5d007f8cf3f254a8649f03d00f
KellenKolbeck <kellengkolbeck@gmail.com>
2016-02-10 09:46:53 -0800

    Add basic page layout

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var quotes = {
-
+	{quote: "111", author: "Kellen Kolbeck"};
+	{quote: "222", author: "Kellen Kolbeck"};
+	{quote: "333", author: "Kellen Kolbeck"};
+	{quote: "444", author: "Kellen Kolbeck"};
+	{quote: "555", author: "Kellen Kolbeck"};
+	{quote: "666", author: "Kellen Kolbeck"};
+	{quote: "777", author: "Kellen Kolbeck"};
+	{quote: "888", author: "Kellen Kolbeck"};
+	{quote: "999", author: "Kellen Kolbeck"};
 }
 
 var getRandomQuote = {
 
 	init: function() {
-		
+		this.clickButton();
 	}
+
+	clickButton: function() {
+		$("get-quote-button").on('click', function() {
+			
+
+			
+		}
+	});
 }
