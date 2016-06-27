134a30b05e7d0ebc7a9705d19d6e05188c463d30
KellenKolbeck <kellengkolbeck@gmail.com>
2016-06-27 16:16:16 -0700

    Add JS code to have random quote with random background color appear when get quote button is pushed

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-var quotes = {
-	{quote: "111", author: "Kellen Kolbeck"};
-	{quote: "222", author: "Kellen Kolbeck"};
-	{quote: "333", author: "Kellen Kolbeck"};
-	{quote: "444", author: "Kellen Kolbeck"};
-	{quote: "555", author: "Kellen Kolbeck"};
-	{quote: "666", author: "Kellen Kolbeck"};
-	{quote: "777", author: "Kellen Kolbeck"};
-	{quote: "888", author: "Kellen Kolbeck"};
-	{quote: "999", author: "Kellen Kolbeck"};
-}
+var quotes = [
+    {quote: "This is pretty darn cool a", author: "Kellen A"},
+    {quote: "This is pretty dang cool b ", author: "Kellen b"},
+    {quote: "This is pretty damn cool c ", author: "Kellen c"},
+    {quote: "This is pretty darn tootin d ", author: "Kellen d"},
+    {quote: "This is pretty freakin e ", author: "Kellen e"},
+    {quote: "This is pretty funkin f ", author: "Kellen f"},
+    {quote: "This is pretty wicked g ", author: "Kellen g"},
+    {quote: "This is pretty h", author: "Kellen h"},
+    {quote: "This is pretty i", author: "Kellen i"},
+];
 
 var getRandomQuote = {
 
-	init: function() {
-		this.clickButton();
-	}
+    init: function() {
+        this.colorChange();
+        this.getQuote();
+    },
 
-	clickButton: function() {
-		$("get-quote-button").on('click', function() {
-			
+    colorChange: function() {
+        $(".get-quote-button").on('click', function() {
+                var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+                $(".container").css("background-color", randomColor);
+            });
+    },
 
-			
-		}
-	});
-}
+    getQuote: function() {
+        $(".get-quote-button").on("click", function() {
+            var randomNum = Math.floor(Math.random() * quotes.length);
+            var randomQuote = quotes[randomNum].quote;
+            $(".quote").html(randomQuote);
+            $(".author").html(quotes[randomNum].author);
+        });
+    }
+};
+
+getRandomQuote.init();
